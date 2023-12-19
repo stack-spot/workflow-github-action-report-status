b47893c7289ce06cabdcd55847b80ac071f891d3
pedrohrfz <pedro.freitas@zup.com.br>
2023-12-18 23:21:20 -0300

    fix url call

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ completed_at=$(date +"%Y-%m-%d %T%z");
 workflow_service="$workflow_url/executions/$execution_id/workflows/steps"
 idm_service="$idm_url/realms/$realm/protocol/openid-connect/token"
 
-secret_stk_login=$(curl --location --request POST $idm_service \
+secret_stk_login=$(curl --location --request POST "$idm_service" \
     --header "Content-Type: application/x-www-form-urlencoded" \
     --data-urlencode "client_id=$client_id" \
     --data-urlencode "grant_type=client_credentials" \
@@ -23,13 +23,13 @@ secret_stk_login=$(curl --location --request POST $idm_service \
 
 if [[ "" -ne "completed" ]]; then
     http_code=$(curl -s -o response.txt -w '%{http_code}' \
-    --location --request PUT $workflow_service \
+    --location --request PUT "$workflow_service" \
     --header "Authorization: Bearer $secret_stk_login" \
     --header 'Content-Type: application/json' \
     --data "{\"name\": \"$status\", \"started_at\": \"$started_at\", \"completed_at\": \"$completed_at\", \"conclusion\": \"$conclusion\"}";)
 else
     http_code=$(curl -s -o response.txt -w '%{http_code}' \
-    --location --request PUT $workflow_service \
+    --location --request PUT "$workflow_service" \
     --header "Authorization: Bearer $secret_stk_login" \
     --header 'Content-Type: application/json' \
     --data "{\"name\": \"$name\", \"status\": \"$status\", \"started_at\": \"$started_at\"}";)
